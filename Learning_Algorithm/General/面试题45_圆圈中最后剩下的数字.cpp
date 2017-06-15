593c9c17095e851c99045c0450dd9928e6cebcfd
bigosprite@163.com <bigosprite@163.com>
2017-06-15 19:53:26 +0800

    add circle list, Q 45

---
+++ Learning_Algorithm/General/面试题45_圆圈中最后剩下的数字.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <list>
+/*
+	面试题43：圆圈中最后剩下的数字
+			0~n-1这n个数字排成一个圆圈，从数字0开始每次从这个圆圈里
+			删除第m个数字。求最后剩下的数字。
+
+	思路：1.使用STL中的list来模拟循环链表
+*/
+
+/*
+ * @功能 圆圈中最后剩下的数字
+ * @param n 数字的个数
+ * @param m 要删除第m个数字
+ *
+ * @复杂度 #1时间复杂度O(m·n);#2空间复杂度O(n)
+ */
+int getLastNumber(int n, int m)
+{
+	if (n < 1 || m < 1)
+		return -1;
+
+	// #2 把0...n-1这些数字存入list
+	std::list<int> numbersList;
+	for (int i = 0; i < n; ++i)
+		numbersList.push_back(i);
+	
+	std::list<int>::iterator current = numbersList.begin();
+	// #1
+	while (numbersList.size() > 1) {
+		for (int i = 1; i < m; ++i) {
+			++current;
+			if (current == numbersList.end())
+				current = numbersList.begin();
+		}
+
+		// 删除（注意erase会使得迭代器失效）
+		// 方法1
+		//auto next = ++current;// 先把指向下一个元素的迭代器保存下来，因为erase迭代器会使得迭代器失效
+		//if (next == numbersList.end())
+		//	next = numbersList.begin();		
+		//--current;// 上行代码不能使用current + 1，它是错误的，因为list不支持随机访问！
+		//numbersList.erase(current);
+		//current = next;
+		// 或者 方法2
+		current = numbersList.erase(current);// OR numbersList.erase(current++);
+		if (current == numbersList.end())
+			current = numbersList.begin();
+	}
+
+	return *current;
+}
